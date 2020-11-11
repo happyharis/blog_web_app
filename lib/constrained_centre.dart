fdffb053b4c3e91eaf3213e7f4419eb72d89545a
Haris <muhamad_haris@hotmail.com>
2020-11-11 13:55:31 +0800

    Add constrained centre widget

---
+++ lib/constrained_centre.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ConstrainedCentre extends StatelessWidget {
+  final Widget child;
+
+  const ConstrainedCentre({Key key, this.child}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Center(
+        child: child,
+      ),
+    );
+  }
+}
