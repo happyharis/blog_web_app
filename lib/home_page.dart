71bab840a9c31e3d125da4d6905a02551b1a72b0
Haris <muhamad_haris@hotmail.com>
2020-11-11 13:55:50 +0800

    Add home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog_web_app/constrained_centre.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,9 +6,67 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        width: 612,
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          width: 612,
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ConstrainedCentre(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://i.ibb.co/ZKkSW4H/profile-image.png'),
+                  radius: 72,
+                ),
+              ),
+              SizedBox(height: 18),
+              ConstrainedCentre(
+                child: SelectableText(
+                  'Flutter Dev',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+              ),
+              SizedBox(height: 40),
+              SelectableText(
+                'Hello, I’m a human. I’m a Flutter developer and an avid human. Occasionally, I nap.',
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+              SizedBox(height: 40),
+              SelectableText(
+                'Blog',
+                style: Theme.of(context).textTheme.headline2,
+              ),
+              BlogListTile()
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class BlogListTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 20),
+        InkWell(
+          child: Text(
+            'What is provider?',
+            style: TextStyle(color: Colors.blueAccent.shade700),
+          ),
+          onTap: () {},
+        ),
+        SizedBox(height: 10),
+        SelectableText(
+          'January 2, 2020',
+          style: Theme.of(context).textTheme.caption,
+        ),
+      ],
     );
   }
 }
